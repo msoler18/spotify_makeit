692e3cd9e5aa96136d24272fb988c5fa79cec94f
Miguel Soler <msoler735@gmail.com>
2017-05-27 21:53:37 -0500

    Add List for Artist And Albums

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+	artist = Artist.create([{name: 'Foo Fighters', image_url: 'https://images.genius.com/18939f486b510212ab9b917daf02152a.1000x798x1.jpg'}])
+	album = Album.create([{name: 'Wasting Light', image_url: 'https://resenassonoras.files.wordpress.com/2011/09/foo_fighters_wasting_light_album_cover.jpg',
+	released_at: '12-04-2011', artist_id: 1}])
+	
